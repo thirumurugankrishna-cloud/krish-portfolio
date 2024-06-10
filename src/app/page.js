516acdb15b8051a6b78ba60308082e8dcc781f3b
Gokul <gokulgk9402@gmail.com>
2024-06-10 15:50:19 +0530

    added code for navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
+import Navbar from "@/components/navbar";
 import { ThemeSwitch } from "@/components/theme-switch";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-24 p-24 bg-background">
-      <ThemeSwitch />
-      <h1 className=" text-foreground font-semibold text-5xl">
+    <main className="flex min-h-screen flex-col items-center bg-background">
+      <Navbar />
+      {/* <h1 className=" text-foreground font-semibold text-5xl">
         Krishna, the bully
-      </h1>
+      </h1> */}
     </main>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ThemeSwitch } from "./theme-switch";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className=" w-full h-[200px] bg-gradient-to-b from-navbar-start to-navbar-end px-[73px] flex items-center justify-between pb-[46px] sticky">
+      <div className=" flex gap-[115px] ">
+        <div className=" flex flex-col">
+          <h2 className=" text-foreground text-xl leading-6 font-medium">
+            Krishna T
+          </h2>
+          <p className=" text-xs text-foreground/50 leading-4 font-normal tracking-[0.125px] ">
+            Product Designer
+          </p>
+        </div>
+
+        <div className=" flex items-center justify-center gap-[70px] font-medium text-2xl leading-5 tracking-[0.2px]">
+          <Link href={"#"}>Work</Link>
+          <Link href={"#"}>About</Link>
+          <Link href={"#"}>Resume</Link>
+        </div>
+      </div>
+
+      <ThemeSwitch />
+    </nav>
+  );
+};
+
+export default Navbar;

38be7ced890d1993f50147068d408e4c5ce93266
Gokul <gokulgk9402@gmail.com>
2024-11-24 19:36:42 +0530

    added projects section, footer section

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,13 @@ import Navbar from "@/components/navbar";
 import Image from "next/image";
 
 import Arrow from "@/resources/arrow.svg";
-import Zee5Pic from "@/resources/Zee5Pic.png";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Home() {
   return (
@@ -36,18 +42,192 @@ export default function Home() {
         </p>
       </section>
 
-      <div className=" h-[100vh] w-full bg-grid-pattern flex flex-col">
-        <h2 className="text-[128px] text-center">SELECTED PROJECTS</h2>
-        <div className=" flex-1 flex gap-8 justify-center items-center ">
-          <Image src={Zee5Pic} />
+      <div className=" w-full bg-grid-pattern flex flex-col h-[1176px] overflow-auto">
+        <div className=" h-[155px] flex items-center justify-center">
+          <h2 className="text-[128px] text-center">SELECTED PROJECTS</h2>
+        </div>
+
+        <div className=" flex items-center w-full justify-center mt-[286px] gap-[138px]">
+          <Image
+            src={"/project-img/timely.png"}
+            height={1030}
+            width={735}
+            alt="Timely project preview"
+          />
+          <div className=" w-[598px] flex gap-[70px]">
+            <div className=" w-[5px] bg-white border relative ml-[9px] ">
+              <div className=" w-[45px] h-[30px] bg-white"></div>
+              <div className="  project-circle rounded-full bg-white absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2"></div>
+            </div>
+            <div>
+              <h3 className=" text-2xl font-medium">
+                {"Immersive Interfaces for Children's Education"}
+              </h3>
+              <p className=" mt-8 text-2xl text-secondary-foreground">
+                Designed interactive educational games to enhance student
+                engagement and learning outcomes.Researched game mechanics,
+                developed educational content, and aligned games with curriculum
+                objectives. Aimed to enhance the virtual learning experience for
+                students.
+              </p>
+              <p className=" mt-[190px] text-2xl text-secondary-foreground">
+                #immersive design #extended reality #interaction design #game
+                design
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className=" flex items-center w-full justify-center mt-[296px] gap-[138px]">
+          <div className=" w-[598px] flex gap-[70px]">
+            <div>
+              <h3 className=" text-2xl font-medium">{"Design System"}</h3>
+              <p className=" mt-8 text-2xl text-secondary-foreground">
+                {
+                  "Contributed to ZEE's design system team, focusing on essential components and ensuring consistency across all products, while bridging the gap between designers and developers. Collaborated on developing design tokens, components, and a visual library."
+                }
+              </p>
+              <p className=" mt-[190px] text-2xl text-secondary-foreground ">
+                #immersive design #extended reality #interaction design #game
+                design
+              </p>
+            </div>
+
+            <div className=" w-[5px] bg-white border relative mr-[9px] ">
+              <div className=" w-[45px] h-[30px] bg-white"></div>
+              <div className=" project-circle rounded-full bg-white absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2"></div>
+            </div>
+          </div>
+
+          <Image
+            src={"/project-img/zee5-design-system.png"}
+            height={1030}
+            width={735}
+            alt="Timely project preview"
+          />
+        </div>
+
+        <div className=" flex items-center w-full justify-center mt-[296px] gap-[138px]">
+          <Image
+            src={"/project-img/timely.png"}
+            height={1030}
+            width={735}
+            alt="Timely project preview"
+          />
+
+          <div className=" w-[598px] flex gap-[70px]">
+            <div className=" w-[5px] bg-white border relative ml-[9px] ">
+              <div className=" w-[45px] h-[30px] bg-white"></div>
+              <div className=" project-circle rounded-full bg-white absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2"></div>
+            </div>
+            <div>
+              <h3 className=" text-2xl font-medium">{"Timely"}</h3>
+              <p className=" mt-8 text-2xl text-secondary-foreground">
+                Timely is an productivity app designed to help collage students
+                bridge their personal and academic goals, by increasing
+                accountability of their activities towards their goals.
+              </p>
+              <p className=" mt-[190px] text-2xl text-secondary-foreground ">
+                #interaction design #visual design #app design #research
+              </p>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className=" h-[100vh] w-full bg-grid-pattern"></div>
+      <section className="   mt-[150px] w-[80vw]">
+        <h2 className=" text-[48px] w-full border-b border-white pb-[26px]">
+          OTHER PROJECTS
+        </h2>
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger className=" text-[40px]">
+              Designing for the vision impaired
+            </AccordionTrigger>
+            <AccordionContent className=" flex gap-12 py-8">
+              <Image
+                src={"/project-img/accordion-1.png"}
+                height={257}
+                width={376}
+                alt="Designing for the vision impaired"
+              />
+              <p className=" text-2xl text-[#BFBFBF]">
+                This project focuses on helping blind users perceive different
+                currencies. The aim is to build a tangible product which can
+                help them detect different currencies. This was done as a part
+                of the course - Tangible User Interfaces
+              </p>
+            </AccordionContent>
+          </AccordionItem>
 
-      {/* <h1 className=" text-foreground font-semibold text-5xl">
-        Krishna, the bully
-      </h1> */}
+          <AccordionItem value="item-2">
+            <AccordionTrigger className=" text-[40px]">
+              File Manager
+            </AccordionTrigger>
+            <AccordionContent className=" flex gap-12 py-8">
+              <Image
+                src={"/project-img/accordion-1.png"}
+                height={257}
+                width={376}
+                alt="Designing for the vision impaired"
+              />
+              <p className=" text-2xl text-[BFBFBF]">
+                This project focuses on helping blind users perceive different
+                currencies. The aim is to build a tangible product which can
+                help them detect different currencies. This was done as a part
+                of the course - Tangible User Interfaces
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value="item-3">
+            <AccordionTrigger className=" text-[40px]">
+              Agrivison4U Website
+            </AccordionTrigger>
+            <AccordionContent className=" flex gap-12 py-8">
+              <Image
+                src={"/project-img/accordion-1.png"}
+                height={257}
+                width={376}
+                alt="Designing for the vision impaired"
+              />
+              <p className=" text-2xl text-[BFBFBF]">
+                This project focuses on helping blind users perceive different
+                currencies. The aim is to build a tangible product which can
+                help them detect different currencies. This was done as a part
+                of the course - Tangible User Interfaces
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </section>
+
+      <section className=" mt-[100px] h-screen"></section>
+
+      <footer className=" bg-[#101010] border-t-2 border-[#808080] h-[50vh] w-full flex justify-around items-center  ">
+        <div className=" w-[492px] space-y-[22px]">
+          <p className=" text-[40px] text-center">
+            {"The Best is yet to come. Let's create it together"}
+          </p>
+          <p className=" italic text-center text-sm tracking-wider">
+            Drop me an email
+          </p>
+          <p className=" text-sm text-center">thirumurugankrishna@gmail.com</p>
+        </div>
+        <div className=" flex gap-[88px]">
+          <div className=" space-y-8">
+            <h4 className=" text-[#808080]">NAVIGATION</h4>
+            <p className=" text-2xl ">Work</p>
+            <p className=" text-2xl ">About</p>
+          </div>
+          <div className=" space-y-8">
+            <h4 className=" text-[#808080]">PROJECTS</h4>
+            <p className=" text-2xl ">Immersive Education</p>
+            <p className=" text-2xl ">Clootrack</p>
+            <p className=" text-2xl ">ZEE5 Design System</p>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,24 +5,20 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className=" w-full h-[20vh] bg-gradient-to-b from-navbar-start/70 to-navbar-end/0 px-[73px] flex items-center justify-between pb-[46px] sticky">
-      <div className=" flex gap-[115px] ">
-        <div className=" flex flex-col">
-          <h2 className=" text-foreground text-xl leading-6 font-medium">
-            Krishna T
-          </h2>
-          <p className=" text-xs text-foreground/50 leading-4 font-normal tracking-[0.125px] ">
-            Product Designer
-          </p>
-        </div>
-
-        <div className=" flex items-center justify-center gap-[70px] font-medium text-2xl leading-5 tracking-[0.2px]">
-          <Link href={"#"}>Work</Link>
-          <Link href={"#"}>About</Link>
-          <Link href={"#"}>Resume</Link>
-        </div>
+      <div className=" flex flex-col gap-2">
+        <h2 className=" text-foreground text-2xl leading-6 font-medium">
+          Krishna T
+        </h2>
+        <p className=" text-foreground/50 leading-4 font-normal tracking-[0.125px] ">
+          Product Designer
+        </p>
       </div>
 
-      <ThemeSwitch />
+      <div className=" flex items-center justify-center gap-[70px] font-medium text-xl leading-5 tracking-[0.2px]">
+        <Link href={"#"}>Work</Link>
+        <Link href={"#"}>About</Link>
+        <Link href={"#"}>Resume</Link>
+      </div>
     </nav>
   );
 };

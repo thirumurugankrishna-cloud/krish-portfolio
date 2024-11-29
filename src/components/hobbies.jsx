593d79f6d04a600b8e05a58acf45ca80b5bd79dd
Gokul <gokulgk9402@gmail.com>
2024-11-29 09:25:34 +0530

    reduce image resolutions

--- src/components/hobbies.jsx
+++ src/components/hobbies.jsx
@@ -9,77 +9,77 @@ const HobbiesSection = () => {
           <div className=" flex flex-col gap-8 models-column">
             <Image
               src={"/3d-models/model-1.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 1"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-2.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 2"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-3.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 3"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-4.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 4"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-5.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 5"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-6.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 6"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-7.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 7"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-8.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 8"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-9.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 9"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-10.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 10"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-11.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 11"
               className=" w-full h-auto"
@@ -90,78 +90,78 @@ const HobbiesSection = () => {
           <div className=" flex flex-col gap-8 inktober-column">
             <Image
               src={"/Inktober/frame-1.png"}
-              height={200}
-              width={200}
+              height={150}
+              width={150}
               alt="inktober frame 1"
               className=" w-full h-auto"
             />
             <Image
               src={"/Inktober/frame-2.png"}
-              height={200}
-              width={200}
+              height={150}
+              width={150}
               alt="inktober frame 2"
               className=" w-full h-auto"
             />
             <Image
               src={"/Inktober/frame-3.png"}
-              height={200}
-              width={200}
+              height={150}
+              width={150}
               alt="inktober frame 3"
               className=" w-full h-auto"
             />
             <Image
               src={"/Inktober/frame-4.png"}
-              height={200}
-              width={200}
+              height={150}
+              width={150}
               alt="inktober frame 4"
               className=" w-full h-auto"
             />
             <Image
               src={"/Inktober/frame-5.png"}
-              height={200}
-              width={200}
+              height={150}
+              width={150}
               alt="inktober frame 5"
               className=" w-full h-auto"
             />
             <Image
               src={"/Inktober/frame-6.png"}
-              height={200}
-              width={200}
+              height={150}
+              width={150}
               alt="inktober frame 6"
               className=" w-full h-auto"
             />
             <Image
               src={"/Inktober/frame-7.png"}
-              height={200}
-              width={200}
+              height={150}
+              width={150}
               alt="inktober frame 7"
               className=" w-full h-auto"
             />
             <Image
               src={"/Inktober/frame-8.png"}
-              height={200}
-              width={200}
+              height={150}
+              width={150}
               alt="inktober frame 8"
               className=" w-full h-auto"
             />
             <Image
               src={"/Inktober/frame-9.png"}
-              height={200}
-              width={200}
+              height={150}
+              width={150}
               alt="inktober frame 9"
               className=" w-full h-auto"
             />
             <Image
               src={"/Inktober/frame-10.png"}
-              height={200}
-              width={200}
+              height={150}
+              width={150}
               alt="inktober frame 10"
               className=" w-full h-auto"
             />
             <Image
               src={"/Inktober/frame-11.png"}
-              height={200}
-              width={200}
+              height={150}
+              width={150}
               alt="inktober frame 11"
               className=" w-full h-auto"
             />
@@ -171,77 +171,77 @@ const HobbiesSection = () => {
           <div className=" flex flex-col gap-8 models-column">
             <Image
               src={"/3d-models/model-1.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 1"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-2.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 2"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-3.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 3"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-4.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 4"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-5.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 5"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-6.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 6"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-7.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 7"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-8.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 8"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-9.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 9"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-10.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 10"
               className=" w-full h-auto"
             />
             <Image
               src={"/3d-models/model-11.png"}
-              height={200}
+              height={120}
               width={200}
               alt="3d-models frame 11"
               className=" w-full h-auto"

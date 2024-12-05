e5be7a54d5f0136aa65a5857e5e162d3537cb764
Gokul <gokulgk9402@gmail.com>
2024-12-05 10:53:57 +0530

    added experience section in about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,10 @@
+import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import { Globe } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+
+import { FaAppStoreIos, FaAndroid } from "react-icons/fa";
 
 const AboutPage = () => {
   return (
@@ -46,7 +50,108 @@ const AboutPage = () => {
             </div>
           </div>
         </section>
+
+        <div className=" my-20 bg-[#424242] px-10  w-full h-[1px]"></div>
+
+        <section className=" space-y-12">
+          <div className=" flex gap-2 items-center">
+            <div className=" size-3 rounded-full bg-white  shadow-[0px_0px_20px_0px]"></div>
+            <h2 className=" text-lg text-[#808080]">EXPERIENCE</h2>
+          </div>
+          <div className=" grid grid-cols-2 gap-[72px]">
+            <div className=" space-y-4">
+              <h3 className=" text-foreground text-4xl">Clootrack</h3>
+              <p className=" text-secondary-foreground text-2xl">Bangalore</p>
+              <Globe className=" text-white size-8" />
+            </div>
+            <div className=" ">
+              <h3 className=" mb-3 text-2xl text-foreground">
+                Product Designer
+              </h3>
+              <p className=" mb-6 text-teritary-foreground text-xl">
+                06/&apos;24 - Present
+              </p>
+              <p className=" text-teritary-foreground text-xl">
+                Translated concepts into Figma prototypes, driving efficient
+                productization. Designed Genie, a GPT-4 chatbot enabling data
+                exploration beyond dashboards. Built a cohesive design system to
+                unify design language and streamline handoffs.
+              </p>
+            </div>
+
+            <div className=" space-y-4">
+              <h3 className=" text-foreground text-4xl">ZEE Entertainment</h3>
+              <p className=" text-secondary-foreground text-2xl">Bangalore</p>
+              <div className=" flex gap-6">
+                <Globe className=" text-white size-8" />
+                <FaAndroid className=" text-white size-8" size={32} />
+                <FaAppStoreIos className=" text-white size-8" size={32} />
+              </div>
+            </div>
+            <div className=" ">
+              <h3 className=" mb-3 text-2xl text-foreground">
+                Associate Product Designer, Design Systems
+              </h3>
+              <p className=" mb-6 text-teritary-foreground text-xl">
+                06/&apos;23 - 03/&apos;24
+              </p>
+              <p className=" text-teritary-foreground text-xl">
+                Contributed to the ZEE design system team, focusing on essential
+                components and ensuring consistency across all products.
+                Collaborated on developing design tokens, components, and a
+                visual library. Created screens for Gen AI projects and ILT20
+                season 2.
+              </p>
+            </div>
+
+            <div className=" space-y-4">
+              <h3 className=" text-foreground text-4xl">Agrivision4U</h3>
+              <p className=" text-secondary-foreground text-2xl">Remote</p>
+              <Globe className=" text-white size-8" />
+            </div>
+            <div className=" ">
+              <h3 className=" mb-3 text-2xl text-foreground">
+                UX Design Intern, Website Designer
+              </h3>
+              <p className=" mb-6 text-teritary-foreground text-xl">
+                Summer &apos;21
+              </p>
+              <p className=" text-teritary-foreground text-xl">
+                Interned as a UI designer at AgriVision4U, conducting research
+                to inform the website&apos;s UI design. Created intuitive,
+                visually appealing UI elements aligned with the brand and target
+                audience, ensuring a seamless user experience.
+              </p>
+            </div>
+
+            <div className=" space-y-4">
+              <h3 className=" text-foreground text-4xl">
+                Indian Institute of Technology
+              </h3>
+              <p className=" text-secondary-foreground text-2xl">Guwahati</p>
+            </div>
+            <div className=" ">
+              <h3 className=" mb-3 text-2xl text-foreground">B.Des</h3>
+              <p className=" mb-6 text-teritary-foreground text-xl">
+                07/&apos;19 - 05/&apos;23
+              </p>
+              <p className=" text-teritary-foreground text-xl">
+                During my four years at IIT Guwahati, I delved into diverse
+                design domains, gaining a well-rounded foundation in the field.
+                For my bachelor&apos;s project, I designed an immersive learning
+                experience for children, integrating AR, VR, and gamification to
+                make education both engaging and interactive. This experience
+                enhanced my expertise in user experience design and fueled my
+                passion for crafting innovative, impactful solutions.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section className=" my-24"></section>
       </main>
+
+      <Footer />
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,9 @@ module.exports = {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
+        teritary: {
+          foreground: "hsl(var(--tertiary-foreground))",
+        },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",

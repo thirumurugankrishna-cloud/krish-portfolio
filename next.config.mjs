faf5125c7d250e4747783b1b6a0733b36c42634c
Gokul <gokulgk9402@gmail.com>
2024-12-25 19:27:31 +0530

    added resume

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/resume.pdf",
+        destination: "/Krishna_Resume.pdf",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
